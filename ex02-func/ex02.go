b89d560f727cad31af66d7f66a6b6a0e126a5f8a
jawher <jawher.moussa@gmail.com>
2016-01-11 12:00:52 +0100

    add a multi-return function excercise

--- ex02-func/ex02.go
+++ ex02-func/ex02.go
@@ -1,7 +1,13 @@
 package ex02
 
-//TODO: déclarer une fonction sum qui accepte 2 entiers et qui retourne leur somme
+//TODO 1: déclarer une fonction "sum" qui accepte 2 entiers et qui retourne leur somme
 
 func sum(a, b int) int {
 	return a + b
+}
+
+// TODO 2: déclarer une fonction "predSucc" qui accepte un entier et qui renvoie son prédecesseur et son successeur
+
+func predSucc(x int) (int, int) {
+	return x - 1, x + 1
 }
