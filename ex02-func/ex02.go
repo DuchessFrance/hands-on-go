a8cd87988ff809a8baeb2cad22465f9a8c918465
Katia Aresti <katiaaresti@gmail.com>
2016-01-12 12:47:53 +0100

    Corrected test and todo for ex02

--- ex02-func/ex02.go
+++ ex02-func/ex02.go
@@ -1,6 +1,6 @@
 package ex02
 
-//TODO 1: déclarer une fonction "sum" qui accepte 2 entiers et qui retourne leur somme
+// TODO 1: déclarer une fonction "sum" qui accepte 2 entiers et qui retourne leur somme
 
 func sum(a, b int) int {
 	return a + b
--- ex02-func/ex02_test.go
+++ ex02-func/ex02_test.go
@@ -8,3 +8,13 @@ func TestSum(t *testing.T) {
 		t.Fatalf("Incorrect result for 4+7: wanted 11, got %d", actual)
 	}
 }
+
+func TestPredSucc(t *testing.T) {
+	prev, next := predSucc(4)
+	if prev != 3 {
+		t.Fatalf("Incorrect result for predSucc.prev: wanted 3, got %d", prev)
+	}
+	if next != 5 {
+		t.Fatalf("Incorrect result for predSucc.next: wanted 5, got %d", next)
+	}
+}
